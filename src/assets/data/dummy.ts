7e934e76052848ecf28c8afdffd1a5c41e675063
FelienZ <rajabfadh@gmail.com>
2026-06-29 09:36:02 +0700

    Add icons & dummyData for gamelist

---
+++ src/assets/data/dummy.ts
@@ -0,0 +1,42 @@
+export const dummyList = [
+    {
+        "id": 1,
+        "title": "Mobile Legends: Bang-bang",
+        "viewers" : 100
+    },
+    {
+        "id": 2,
+        "title": "Genshin Impact",
+        "viewers" : 75
+    },
+    {
+        "id": 3,
+        "title": "Zenless Zone Zero",
+        "viewers" : 60
+    },
+    {
+        "id": 4,
+        "title": "Residen Evil Requiem",
+        "viewers" : 50
+    },
+    {
+        "id": 5,
+        "title": "Valorant",
+        "viewers" : 45
+    },
+    {
+        "id": 6,
+        "title": "Neko Michi",
+        "viewers" : 40
+    },
+    {
+        "id": 7,
+        "title": "GTA V Enchanced",
+        "viewers" : 35
+    },
+    {
+        "id": 8,
+        "title": "Wuthering Waves",
+        "viewers" : 30
+    }
+]
