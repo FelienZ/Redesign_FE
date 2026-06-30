dce209ab638c5b121604b37f96172fc025fdaf92
FelienZ <rajabfadh@gmail.com>
2026-06-30 21:55:18 +0700

    feat(utils): create style config for className, etc.

---
+++ src/utils/styleConfig.ts
@@ -0,0 +1,5 @@
+export const newsConfig = {
+    ["Pengumuman Penting"] : {className: "bg-(--rating-18-soft) text-(--rating-18-text)"},
+    ["Berita"] : {className: "bg-(--rating-3-soft) text-(--rating-3-text)"},
+    ["Panduan"] : {className: "bg-secondary text-primary"},
+}
