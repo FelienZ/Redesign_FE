1fce56dbc3f9858b78994af309a1ca4e4a052e1c
FelienZ <rajabfadh@gmail.com>
2026-06-30 21:57:56 +0700

    feat(root): implement router provider in root component

--- src/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import HeroSection from "./custom/hero";
-
-export default function Dashboard() {
-  return (
-    <section className="">
-      <HeroSection />
-    </section>
-  );
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "@/index.css";
 import { RouterProvider } from "react-router";
-import { routerConfig } from "./utils/RouterProvider";
+import { routerConfig } from "@/utils/RouterProvider";
 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={routerConfig} />,
