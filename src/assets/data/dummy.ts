d8c00d8c714bce7dfbc331f28c0c8bd17fe32d6b
FelienZ <rajabfadh@gmail.com>
2026-06-30 21:50:04 +0700

    chore(assets): create dummy data

--- src/assets/data/dummy.ts
+++ src/assets/data/dummy.ts
@@ -40,3 +40,152 @@ export const dummyList = [
         "viewers" : 30
     }
 ]
+
+export const dummyRating = [
+    {
+        "rating": "3+",
+        "count" : 3794,
+        "description" : "Jumlah Permainan Rating Usia 3 Tahun Keatas",
+        "backgroundColor": "bg-(--rating-3-solid)"
+    },
+    {
+        "rating": "7+",
+        "count" : 17,
+        "description" : "Jumlah Permainan Rating Usia 7 Tahun Keatas",
+        "backgroundColor": "bg-(--rating-7-solid)"
+    },
+    {
+        "rating": "13+",
+        "count" : 2591,
+        "description" : "Jumlah Permainan Rating Usia 13 Tahun Keatas",
+        "backgroundColor": "bg-(--rating-13-solid)"
+    },
+    {
+        "rating": "15+",
+        "count" : 298,
+        "description" : "Jumlah Permainan Rating Usia 15 Tahun Keatas",
+        "backgroundColor": "bg-(--rating-15-solid)"
+    },
+    {
+        "rating": "18+",
+        "count" : 1486,
+        "description" : "Jumlah Permainan Rating Usia 18 Tahun Keatas",
+        "backgroundColor": "bg-(--rating-18-solid)"
+    },
+]
+
+export const dummyThumbnail = [
+    {
+        "id" : 1,
+        "title" : "Genshin Impact",
+        "rating" : 18,
+        "genre": "RPG",
+        "imageUrl": "Genshin.png"
+    },
+    {
+        "id" : 2,
+        "title" : "Zenless Zone Zero",
+        "rating" : 18,
+        "genre": "RPG",
+        "imageUrl": "zzz.webp"
+    },
+    {
+        "id" : 3,
+        "title" : "Honkai: Star Rail",
+        "rating" : 15,
+        "genre": "RPG",
+        "imageUrl": "Honkai_Star_Rail_App.webp"
+    },
+    {
+        "id" : 4,
+        "title" : "Valorant",
+        "rating" : 13,
+        "genre": "FPS",
+        "imageUrl": "valo.jpg"
+    },
+    {
+        "id" : 5,
+        "title" : "Mobile Legends: Bang-bang",
+        "rating" : 7,
+        "genre": "Multi Player",
+        "imageUrl": "mlbb.jpg"
+    },
+    {
+        "id" : 6,
+        "title" : "Neko Michi",
+        "rating" : 3,
+        "genre": "Simulation",
+        "imageUrl": "nekomichi.jpg"
+    },
+    {
+        "id" : 7,
+        "title" : "Neko Michi",
+        "rating" : 3,
+        "genre": "Simulation",
+        "imageUrl": "nekomichi.jpg"
+    },
+    {
+        "id" : 8,
+        "title" : "Neko Michi",
+        "rating" : 3,
+        "genre": "Simulation",
+        "imageUrl": "nekomichi.jpg"
+    },
+    {
+        "id" : 9,
+        "title" : "Neko Michi",
+        "rating" : 3,
+        "genre": "Simulation",
+        "imageUrl": "nekomichi.jpg"
+    },
+    {
+        "id" : 10,
+        "title" : "Neko Michi",
+        "rating" : 3,
+        "genre": "Simulation",
+        "imageUrl": "nekomichi.jpg"
+    },
+]
+
+export const dummyNews = [
+    {
+        "id" : 1,
+        "title" : "Evaluasi dan Penangguhan Sementara Layanan Klasifikasi Gim IGRS",
+        "label": "Pengumuman Penting",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptas quos. Tempora debitis voluptatum iure, repudiandae eius fugiat ipsa, commodi voluptate blanditiis dicta vero deserunt. Libero animi voluptatibus esse et?",
+        "createdAt" : "2026-06-30T13:25:00Z",
+        "imageUrl" : "/nekomichi.jpg"
+    },
+    {
+        "id" : 2,
+        "title" : "Pemberitahuan Terkait Proses Verifikasi IGRS",
+        "label": "Pengumuman Penting",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptas quos. Tempora debitis voluptatum iure, repudiandae eius fugiat ipsa, commodi voluptate blanditiis dicta vero deserunt. Libero animi voluptatibus esse et?",
+        "createdAt" : "2026-06-30T13:25:00Z",
+        "imageUrl" : "/nekomichi.jpg"
+    },
+    {
+        "id" : 3,
+        "title" : "Maklumat Pelayanan IGRS untuk Masyarakat",
+        "label": "Berita",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptas quos. Tempora debitis voluptatum iure, repudiandae eius fugiat ipsa, commodi voluptate blanditiis dicta vero deserunt. Libero animi voluptatibus esse et?",
+        "createdAt" : "2026-06-30T13:25:00Z",
+        "imageUrl" : "/nekomichi.jpg"
+    },
+    {
+        "id" : 4,
+        "title" : "Panduan Cuplikan Konten untuk Penerbit Gim",
+        "label": "Panduan",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptas quos. Tempora debitis voluptatum iure, repudiandae eius fugiat ipsa, commodi voluptate blanditiis dicta vero deserunt. Libero animi voluptatibus esse et?",
+        "createdAt" : "2026-06-30T13:25:00Z",
+        "imageUrl" : "/nekomichi.jpg"
+    },
+    {
+        "id" : 5,
+        "title" : "IGRS Hadirkan Fitur Pencarian Gim Berdasarkan Usia",
+        "label": "Berita",
+        "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptas quos. Tempora debitis voluptatum iure, repudiandae eius fugiat ipsa, commodi voluptate blanditiis dicta vero deserunt. Libero animi voluptatibus esse et?",
+        "createdAt" : "2026-06-30T13:25:00Z",
+        "imageUrl" : "/nekomichi.jpg"
+    },
+] 
