05d6230c7759b88ef5f3d27ae3081c4e99bdf428
FelienZ <rajabfadh@gmail.com>
2026-07-01 19:24:04 +0700

    feat(utils): create decoration Images definition, new dir for services, etc.

---
+++ src/utils/decorationItems.ts
@@ -0,0 +1,48 @@
+import Ghost from "@/assets/elements/Ghost.svg";
+import Star from "@/assets/elements/Star.svg";
+
+export const decorationItems = [
+    {
+        src: Ghost,
+        top: "25%",
+        left: "8%",
+        size: 45,
+        opacity: .3
+    },
+    {
+        src: Ghost,
+        top: "42%",
+        left: "90%",
+        size: 70,
+        opacity: .8
+    },
+    {
+        src: Star,
+        top: "18%",
+        left: "14%",
+        size: 12,
+        opacity: .8
+    },
+    {
+        src: Star,
+        top: "70%",
+        left: "10%",
+        size: 10,
+        opacity: .2
+    },
+    {
+        src: Star,
+        top: "35%",
+        left: "80%",
+        size: 12,
+        opacity: .2
+    },
+    //Paling bawah kanan star
+    {
+        src: Star,
+        top: "80%",
+        left: "85%",
+        size: 12,
+        opacity: .8
+    },
+]
