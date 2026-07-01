2d76394ccae1434d6fc400ffa64b3b0d56b63b9c
FelienZ <rajabfadh@gmail.com>
2026-07-01 16:23:51 +0700

    refactor(pages): change components import path in dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,9 @@
-import HeroSection from "@/custom/heroSection";
-import RatingSection from "@/custom/ratingSection";
-import AboutSection from "@/custom/aboutSection";
-import GameSection from "@/custom/gameSection";
-import NewsSection from "@/custom/newsSection";
-import AddSection from "@/custom/addSection";
+import HeroSection from "@/components/custom/heroSection";
+import RatingSection from "@/components/custom/ratingSection";
+import AboutSection from "@/components/custom/aboutSection";
+import GameSection from "@/components/custom/gameSection";
+import NewsSection from "@/components/custom/newsSection";
+import AddSection from "@/components/custom/addSection";
 import Footer from "@/layout/footer";
 
 export default function Dashboard() {
