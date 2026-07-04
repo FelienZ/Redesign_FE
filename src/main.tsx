60ccba126f7d6641cd4cc6dc35e1f39923492156
FelienZ <rajabfadh@gmail.com>
2026-07-04 19:05:21 +0700

    feat(main): implement queryClient Provider in entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import { RouterProvider } from "react-router";
 import { routerConfig } from "@/utils/RouterProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/libs/queryClient";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={routerConfig} />,
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={routerConfig} />
+  </QueryClientProvider>,
 );
