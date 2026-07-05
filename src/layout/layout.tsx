0760b8c9bc0d10ac7fdf514d698128bd8cba93af
FelienZ <rajabfadh@gmail.com>
2026-07-06 06:43:48 +0700

    feat(layout): implement scroll helper

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
 import Navbar from "@/layout/navbar";
 import { LanguageProvider } from "@/utils/LanguageContext";
+import ScrollHelper from "@/utils/ScrollHelper";
 
 export default function Layout() {
+  ScrollHelper();
   return (
     <LanguageProvider>
       <section className="bg-background">
