d8cef8ad684123b328526bf5fdc844958309ff42
FelienZ <rajabfadh@gmail.com>
2026-07-06 06:38:22 +0700

    fix(services): Fixing missing await keyword in async services & refer services to contract

--- src/utils/services/gameServices.ts
+++ src/utils/services/gameServices.ts
@@ -1,27 +1,36 @@
 import { ApiClient } from "../libs/api";
-import type { GetGamesQueryParams } from "../types/gameQueryParams";
+import type { ApiResponse } from "../types/ApiResponse";
+import type { GetGamesQueryParams } from "../types/games/gameQueryParams";
+import type { Developer, Game, GameCards, Genre, Platform, Publisher, Rating } from "../types/games/Games";
 
 export const GameServices = {
     //handler search, semua yg via params: ex. /games?search=genshin, /games?genre=rpg
-    getGameList: async(params:GetGamesQueryParams) => {
-        return ApiClient.get('/games', {params})
+    getGameList: async(params:GetGamesQueryParams): Promise<ApiResponse<GameCards[]>> => {
+        const response = await ApiClient.get('/games', {params})
+        return response.data
     },
-    getGameDetail: async(slug: string) => {
-        return ApiClient.get(`/games/${slug}`)
+    getGameDetail: async(slug: string): Promise<Game>  => {
+        const response = await ApiClient.get(`/games/${slug}`)
+        return response.data
     },
-    getGenres: async() => {
-        return ApiClient.get('/games/genres')
+    getGenres: async(): Promise<Genre[]> => {
+        const response = await ApiClient.get('/games/genres')
+        return response.data
     },
-    getPlatforms: async() => {
-        return ApiClient.get('/games/platforms')
+    getPlatforms: async(): Promise<Platform[]> => {
+        const response = await ApiClient.get('/games/platforms')
+        return response.data
     },
-    getDevelopers: async() => {
-        return ApiClient.get('/games/developers')
+    getDevelopers: async(): Promise<Developer[]> => {
+        const response = await ApiClient.get('/games/developers')
+        return response.data
     },
-    getPublishers: async() => {
-        return ApiClient.get('/games/publishers')
+    getPublishers: async(): Promise<Publisher> => {
+        const response = await ApiClient.get('/games/publishers')
+        return response.data
     },
-    getRatings: async() => {
-        return ApiClient.get('/games/ratings')
+    getRatings: async(): Promise<Rating[]> => {
+        const response = await ApiClient.get('/games/ratings')
+        return response.data
     },
 }
--- src/utils/services/newsServices.ts
+++ src/utils/services/newsServices.ts
@@ -1,14 +1,19 @@
 import { ApiClient } from "../libs/api";
-import type { NewsQueryParams } from "../types/newsQueryParams";
+import type { ApiResponse } from '../types/ApiResponse';
+import type { News, NewsCategory } from "../types/news/News";
+import type { GetNewsQueryParams } from "../types/news/newsQueryParams";
 
 export const NewsServices = {
-    getNewsList: async(params: NewsQueryParams) => {
-        return ApiClient.get(`/news`, {params})
+    getNewsList: async(params: GetNewsQueryParams): Promise<ApiResponse<News[]>> => {
+        const response = await ApiClient.get(`/news`, {params})
+        return response.data
     },
-    getNewsDetail: async(slug: string) => {
-        return ApiClient.get(`news/${slug}`)
+    getNewsDetail: async(slug: string): Promise<ApiResponse<News[]>> => {
+        const response = await ApiClient.get(`news/${slug}`)
+        return response.data
     },
-    getCategories: async() => {
-        return ApiClient.get(`/news/categories`)
+    getCategories: async(): Promise<ApiResponse<NewsCategory>> => {
+        const response = await ApiClient.get(`/news/categories`)
+        return response.data
     }, 
 }
